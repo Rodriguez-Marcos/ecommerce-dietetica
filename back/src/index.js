c7f6e63738353a3909ffc6950034e43796ff970f
Davoyandun <yandundavid@gmail.com>
2021-09-27 14:01:48 -0500

    prueba de pull req

--- back/src/index.js
+++ back/src/index.js
@@ -2,12 +2,13 @@ import app from './app.js';
 import { sequelize } from './database/db.js'
 import '@babel/polyfill'
 
-// function main() {
-//     sequelize.sync({ force: true }).then(() => {
-//         app.listen(3001, () => {
-//             console.log('listening on port 3001, prueba' ); // eslint-disable-line no-console
-//         });
-//     })
-// }
+function main() {
+    sequelize.sync({ force: true }).then(() => {
+        app.listen(3001, () => {
+            console.log('listening on port 3001, prueba de merge' ); // eslint-disable-line no-console
+        });
+    })
+}
 
-// main()
+main()
+ 
