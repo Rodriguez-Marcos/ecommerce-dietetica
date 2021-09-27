f7a9ca76fc7f6e22f99cf08d8f4e82ff974ba2b3
Farid Sesin <77751916+fsesin@users.noreply.github.com>
2021-09-27 14:08:41 -0500

    Update index.js

--- back/src/index.js
+++ back/src/index.js
@@ -5,7 +5,7 @@ import '@babel/polyfill'
 function main() {
     sequelize.sync({ force: true }).then(() => {
         app.listen(3001, () => {
-            console.log('listening on port 3001'); // eslint-disable-line no-console
+            console.log('listening on port 3001 hola mundo'); // eslint-disable-line no-console
         });
     })
 }
