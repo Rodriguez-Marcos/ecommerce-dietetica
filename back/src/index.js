e43c45986bbd0e4da72b865eb7229386d9c36ab1
Davoyandun <yandundavid@gmail.com>
2021-09-27 13:48:32 -0500

    push de prueba

--- back/src/index.js
+++ back/src/index.js
@@ -2,12 +2,12 @@ import app from './app.js';
 import { sequelize } from './database/db.js'
 import '@babel/polyfill'
 
-// function main() {
-//     sequelize.sync({ force: true }).then(() => {
-//         app.listen(3001, () => {
-//             console.log('listening on port 3001'); // eslint-disable-line no-console
-//         });
-//     })
-// }
+function main() {
+    sequelize.sync({ force: true }).then(() => {
+        app.listen(3001, () => {
+            console.log('listening on port 3001 , prueba'); // eslint-disable-line no-console
+        });
+    })
+}
 
-// main()
+main()
