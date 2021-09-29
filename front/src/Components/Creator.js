1a332fc624d96d36428e1ed2437d88c6ef03cf57
Davoyandun <yandundavid@gmail.com>
2021-09-29 18:15:20 -0500

    correccion de indentado

--- front/src/Components/Creator.js
+++ front/src/Components/Creator.js
@@ -94,110 +94,115 @@ export default function Creator() {
   }
   return (
     <div>
-      <form onSubmit={(e) => handlerSubmitProduct(e)}>
-        <div>
-          <label>Nombre</label>
-          <input
-            type="text"
-            value={input.name}
-            name="name"
-            onChange={(e) => handlerProduct(e)}
-          />
-          {!input.name ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label>Precio</label>
-          <input
-            type="number"
-            value={input.price}
-            name="price"
-            onChange={(e) => handlerProduct(e)}
-          />
-          {!input.price ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label>Descripción </label>
-          <input
-            type="textarea"
-            value={input.description}
-            name="description"
-            onChange={(e) => handlerProduct(e)}
-          />
-          {!input.description ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label>Stock </label>
-          <input
-            type="number"
-            value={input.stock}
-            min="0"
-            name="stock"
-            onChange={(e) => handlerProduct(e)}
-          />
-          {!input.stock ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label> imagen</label>
-          <input
-            type="file"
-            accept="image/png, .jpeg, .jpg"
-            value={input.image}
-            name="image"
-            onChange={(e) => handlerProduct(e)}
-          />
-          {!input.image ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <button> Crear producto </button>
-      </form>
-
-      <form onSubmit={(e) => handlerSubmitCategory(e)}>
-        <h2> agregar nueva categoria</h2>
-        <div>
-          <label>Nombre de categoria</label>
-          <input
-            name="name"
-            type="text"
-            value={category.name}
-            onChange={(e) => handlerCategory(e)}
-          />
-          {!category.name ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label>Descripción de categoria</label>
-          <input
-            name="description"
-            type="textarea"
-            value={category.description}
-            onChange={(e) => handlerCategory(e)}
-          />
-          {!category.description ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <button> Crear Categoria</button>
-      </form>
-      <form onSubmit={(e) => handlerSubmitDiet(e)}>
-        <h2> agregar nueva Dieta</h2>
-        <div>
-          <label>Nombre de Dieta</label>
-          <input
-            name="name"
-            type="text"
-            value={diet.name}
-            onChange={(e) => handlerDiet(e)}
-          />
-          {!diet.name ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <div>
-          <label>Descripción de la Dieta</label>
-          <input
-            name="description"
-            type="textarea"
-            value={diet.description}
-            onChange={(e) => handlerDiet(e)}
-          />
-          {!diet.description ? <output> ❌</output> : <output> ✔</output>}
-        </div>
-        <button> Crear Dieta</button>
-      </form>
+      <div>
+        <form onSubmit={(e) => handlerSubmitProduct(e)}>
+          <div>
+            <label>Nombre</label>
+            <input
+              type="text"
+              value={input.name}
+              name="name"
+              onChange={(e) => handlerProduct(e)}
+            />
+            {!input.name ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label>Precio</label>
+            <input
+              type="number"
+              value={input.price}
+              name="price"
+              onChange={(e) => handlerProduct(e)}
+            />
+            {!input.price ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label>Descripción </label>
+            <input
+              type="textarea"
+              value={input.description}
+              name="description"
+              onChange={(e) => handlerProduct(e)}
+            />
+            {!input.description ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label>Stock </label>
+            <input
+              type="number"
+              value={input.stock}
+              min="0"
+              name="stock"
+              onChange={(e) => handlerProduct(e)}
+            />
+            {!input.stock ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label> imagen</label>
+            <input
+              type="file"
+              accept="image/png, .jpeg, .jpg"
+              value={input.image}
+              name="image"
+              onChange={(e) => handlerProduct(e)}
+            />
+            {!input.image ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <button> Crear producto </button>
+        </form>
+      </div>
+      <div>
+        <form onSubmit={(e) => handlerSubmitCategory(e)}>
+          <h2> agregar nueva categoria</h2>
+          <div>
+            <label>Nombre de categoria</label>
+            <input
+              name="name"
+              type="text"
+              value={category.name}
+              onChange={(e) => handlerCategory(e)}
+            />
+            {!category.name ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label>Descripción de categoria</label>
+            <input
+              name="description"
+              type="textarea"
+              value={category.description}
+              onChange={(e) => handlerCategory(e)}
+            />
+            {!category.description ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <button> Crear Categoria</button>
+        </form>
+      </div>
+      <div>
+        <form onSubmit={(e) => handlerSubmitDiet(e)}>
+          <h2> agregar nueva Dieta</h2>
+          <div>
+            <label>Nombre de Dieta</label>
+            <input
+              name="name"
+              type="text"
+              value={diet.name}
+              onChange={(e) => handlerDiet(e)}
+            />
+            {!diet.name ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <div>
+            <label>Descripción de la Dieta</label>
+            <input
+              name="description"
+              type="textarea"
+              value={diet.description}
+              onChange={(e) => handlerDiet(e)}
+            />
+            {!diet.description ? <output> ❌</output> : <output> ✔</output>}
+          </div>
+          <button> Crear Dieta</button>
+        </form>
+      </div>
     </div>
   );
 }
