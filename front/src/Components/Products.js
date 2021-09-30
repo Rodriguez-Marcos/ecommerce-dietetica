12bfe2afc23299330de5289c484e2449b613e4d2
Rocio De Luz Jalil <80021870+rociojalil@users.noreply.github.com>
2021-09-29 23:31:09 -0300

    detalle + cards home (#29)

--- front/src/Components/Products.js
+++ front/src/Components/Products.js
@@ -1,7 +1,8 @@
 import React,{useState,useEffect} from 'react';
 import { connect } from 'react-redux';
 import ProductCard from './ProductCard';
-import './productsgrid.css'
+import styles from './product.module.css'
+import { Link } from 'react-router-dom';
 
 export default function ProductsCards( params) {
   
@@ -12,25 +13,52 @@ export default function ProductsCards( params) {
 
   },[])
   return (
-             <div >
-              {products && products.map((product) => (
-                
-                <div  className="Recetario" key={product.id}>
-          <ProductCard
-          image={product.image}
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          price={product.price}
-          description={product.description}
-          stock={product.stock}
-          />
+    <div className={styles.main}>
+      
+        {products && products.map((product) => (
+          <div>
+         
+         <div className={styles.cardContainer}>
+          <div className={styles.cardName}>{product.name}</div>
+          
+          <img className={styles.cardFoto} src={product.image} alt="Not Found"  />
+          <div className={styles.cardAttack}>${product.price}</div>
+          </div>
+          <div className={styles.detail}>
+         <Link  to={`/Detail/${product.id}`} style={{ color: "black", textDecoration: "none" }}>
+         <p>
+           Ver este producto
+          </p>
+         </Link>
+         </div>
+         
+         <button className={styles.boton}>Agregar al carrito</button>
+     
+     
           </div>
           
+          
+      
+         
+        
+              
+         
+          
+      
+          
           ))}
-    </div>
-    
+          </div>
+  
+          
   );
 };
 
 
+ {/* //  key={product.id}>
+          // <ProductCard
+          // image={product.image}
+          // key={product.id}
+          // id={product.id}
+          // name={product.name}
+          // price= {product.price}
+          // stock={product.stock} */}
