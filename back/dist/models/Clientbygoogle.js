54263a91bf63432c92e6bb7b0e680ac4e22c24e1
pablo agustin <39503400+JDFgamer@users.noreply.github.com>
2021-09-30 23:44:59 -0300

    creacion de usuario y usuario by google (#42)

--- back/dist/models/Clientbygoogle.js
+++ back/dist/models/Clientbygoogle.js
@@ -13,25 +13,20 @@ var _Order = _interopRequireDefault(require("./Order.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var Client = _db.sequelize.define('client', {
-  id: {
-    type: _sequelize["default"].INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+var Clientbygoogle = _db.sequelize.define('clientbygoogle', {
+  googleId: {
+    type: _sequelize["default"].TEXT,
+    primaryKey: true
   },
-  name: {
+  givenName: {
     type: _sequelize["default"].TEXT,
     allowNull: false
   },
-  lastname: {
+  familyName: {
     type: _sequelize["default"].TEXT,
     allowNull: false
   },
   email: {
-    type: _sequelize["default"].TEXT,
-    allowNull: false
-  },
-  password: {
     type: _sequelize["default"].TEXT,
     allowNull: false
   },
@@ -49,15 +44,15 @@ var Client = _db.sequelize.define('client', {
   timestamps: false
 });
 
-Client.hasMany(_Order["default"], {
-  foreignKey: 'id_client',
-  sourceKey: 'id'
+Clientbygoogle.hasMany(_Order["default"], {
+  foreignKey: 'googleId_client',
+  sourceKey: 'googleId'
 });
 
-_Order["default"].belongsTo(Client, {
-  foreignKey: 'id_client',
-  sourceKey: 'id'
+_Order["default"].belongsTo(Clientbygoogle, {
+  foreignKey: 'googleId_client',
+  sourceKey: 'googleId'
 });
 
-var _default = Client;
+var _default = Clientbygoogle;
 exports["default"] = _default;
