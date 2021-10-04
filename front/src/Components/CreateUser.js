9bc9e00c819ad6c210564e7ff29e56f15fda4c96
pedro pablo piñeros guerrero <pedropablopinerosg@hotmail.com>
2021-10-04 08:26:27 -0500

    css crear usuario

--- front/src/Components/CreateUser.js
+++ front/src/Components/CreateUser.js
@@ -1,47 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import GoogleLogin from 'react-google-login';
-import {validate} from '../Utils/ValidateUser'
+import { validate } from '../Utils/ValidateUser'
 import { useHistory } from "react-router-dom";
 import './CreateUser.css'
+import { Form, Button } from 'react-bootstrap'
 
 export default function CreateUser() {
 
     const history = useHistory();
 
-   async function createUser(payload){
-            await axios.post("http://localhost:3001/clients", payload)
-            .then( (response) => {console.log(response)})
-            .catch( (err) => console.error(err))
+    async function createUser(payload) {
+        await axios.post("http://localhost:3001/clients", payload)
+            .then((response) => { console.log(response) })
+            .catch((err) => console.error(err))
 
-             };
-    async function createUserByGoogle(payload){
+    };
+    async function createUserByGoogle(payload) {
         await axios.post("http://localhost:3001/clients/bygoogle", payload)
-        .then( (response) => {console.log(response)})
-        .catch( (err) => console.error(err))
+            .then((response) => { console.log(response) })
+            .catch((err) => console.error(err))
 
-         };
-           
+    };
+
 
 
     const [input, setInput] = useState({
-        name:'',
+        name: '',
         lastname: '',
         password: '',
         email: '',
-        address:'peru 30000',
-        phone:'213124124',
+        address: 'peru 30000',
+        phone: '213124124',
     }
     )
 
     const [errors, setErrors] = useState({
-        name:'',
+        name: '',
         lastname: '',
         password: '',
         email: ''
     }
     )
-    
+
 
     function handlerUser(event) {
         setErrors(validate({
@@ -54,72 +55,97 @@ export default function CreateUser() {
         })
     }
 
-        function handelSubmit(event) {
-            event.preventDefault()
-            if(!input.name || !input.lastname || !input.password || !input.email)
-            {
-                alert('Debes llenar todos los campos')
-            }
-            else{
-                createUser(input)
-                alert('Se creo usuario exitosamente')
-                history.push('/home')
-            }
+    function handelSubmit(event) {
+        event.preventDefault()
+        if (!input.name || !input.lastname || !input.password || !input.email) {
+            alert('Debes llenar todos los campos')
         }
+        else {
+            createUser(input)
+            alert('Se creo usuario exitosamente')
+            history.push('/home')
+        }
+    }
 
 
     const responseGoogle = (response) => {
         console.log(response.profileObj);
         createUserByGoogle(response.profileObj)
-        if (response.profileObj !== undefined){
-        history.push('/home')}
-      }
+        if (response.profileObj !== undefined) {
+            history.push('/home')
+        }
+    }
 
     return (
-        <div className="divuser">
-        <p>Nombre</p>
-        <input
-              type="text"
-              value={input.name}
-              name="name"
-              onChange={handlerUser}
-            /> 
-            
-        <p>Apellido</p>
-        <input
-              type="text"
-              value={input.lastname}
-              name="lastname"
-              onChange={handlerUser}
-            /> 
-            
-        <p>Password</p>
-        <input
-              type="password"
-              value={input.password}
-              name="password"
-              onChange={handlerUser}
-            /> 
-           
-        <p>Email</p>
-        <input
-              type="text"
-              value={input.email}
-              name="email"
-              onChange={handlerUser}
-            /> 
+        <Form className="divuser">
+            <Form.Group className="mb-3" controlId="formBasicEmail" >
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Nombre" type="text"
+                    value={input.name}
+                    name="name"
+                    onChange={handlerUser} />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Apellidos</Form.Label>
+                <Form.Control type="text" placeholder="Apellidos" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Crear Cuenta
+            </Button>
+        </Form>
 
-        <button onClick={handelSubmit}> Crear cuenta </button>
 
-      
+        // <div className="divuser">
+        // <p>Nombre</p>
+        // <input
+        //       type="text"
+        //       value={input.name}
+        //       name="name"
+        //       onChange={handlerUser}
+        //     /> 
 
-        <GoogleLogin
-        clientId="908895428836-kaesjl71puimi31fjbffca9t4nvl7v6r.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        />,
-        </div>
-        )
+        // <p>Apellido</p>
+        // <input
+        //       type="text"
+        //       value={input.lastname}
+        //       name="lastname"
+        //       onChange={handlerUser}
+        //     /> 
+
+        // <p>Password</p>
+        // <input
+        //       type="password"
+        //       value={input.password}
+        //       name="password"
+        //       onChange={handlerUser}
+        //     /> 
+
+        // <p>Email</p>
+        // <input
+        //       type="text"
+        //       value={input.email}
+        //       name="email"
+        //       onChange={handlerUser}
+        //     /> 
+
+        // <button onClick={handelSubmit}> Crear cuenta </button>
+
+
+
+        // <GoogleLogin
+        // clientId="908895428836-kaesjl71puimi31fjbffca9t4nvl7v6r.apps.googleusercontent.com"
+        // buttonText="Login"
+        // onSuccess={responseGoogle}
+        // onFailure={responseGoogle}
+        // cookiePolicy={'single_host_origin'}
+        // />,
+        // </div>
+    )
 }
