3f15b6a706bf8b4e61de4471a5414a15079d564e
Juan Lertora <84674766+JuanLertora@users.noreply.github.com>
2021-10-07 02:20:28 -0300

    Rama juancho (#87)
    * act
    
    * act
    
    * act

--- front/src/App.js
+++ front/src/App.js
@@ -14,6 +14,7 @@ import fondo from '../src/image/fondo1.jpg'
 import { UserContextProvider } from './Contexts/UserContext';
 import Login from './Components/Login';
 import Sidebar from './Components/AdminSideBar';
+import UsersAdminDetail from './Components/UsersAdminDetail'
 
 function App() {
   return (
@@ -28,7 +29,8 @@ function App() {
     <Route exact path='/trolley' component={Trolley}/>
     <Route exact path='/CreateUser' component={CreateUser}/>
     <Route exact path='/Login' component={Login}/>
-    <Route path='/Admin' component={Creator}/>
+    <Route exact path='/Admin' component={Creator}/>
+    <Route exact path='/Admin/user' component={UsersAdminDetail}/>
 
 
     </Switch>
