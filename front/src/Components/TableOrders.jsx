4b648af4399f679c0d623a5f7769b582cf64939b
pedro pablo piñeros guerrero <pedropablopinerosg@hotmail.com>
2021-10-08 18:48:40 -0500

    Navbar admin superior y lateral izq responsive

--- front/src/Components/TableOrders.jsx
+++ front/src/Components/TableOrders.jsx
@@ -1,56 +1,63 @@
-import { React } from "react";import  "bootstrap";
+import { React } from "react"; import "bootstrap";
 import {
   Button,
 } from "reactstrap";
+import Topbar from "./AdminTopBar";
+import Sidebar from "./AdminSideBar";
+import './TableOrders.css'
 
-export default function TableOrders({orders, handlerStatus}) {
+export default function TableOrders({ orders, handlerStatus }) {
   return (
-    <div>
-  <table className="table">
-        <thead>
-          <tr>
-            <th>Nro: </th>
-            <th>Datos del cliente:</th>
-            <th>Email</th>
-            <th>Fecha de pedido:</th>
-            <th>Total:</th>
-            <th>Estado:</th>
-       
-            <th>Detalles:</th>
-          </tr>
-        </thead>
+    <div className="ordersContenedor">
+      <Topbar />
+      <div className="ordersTable-Sidebar">
+        <Sidebar />
+        <table className="table1">
+          <thead>
+            <tr>
+              <th>Nro: </th>
+              <th>Datos del cliente:</th>
+              <th>Email</th>
+              <th>Fecha de pedido:</th>
+              <th>Total:</th>
+              <th>Estado:</th>
 
-        {orders ? (
-          orders.map((e) => (
-            <tbody>
-              <td>{e.id}</td>
-              <td>{e.client? e.client.name: 'User' } {'  '}{e.client? e.client.lastname: 'No Found ' } </td>
-              <td>{e.client? e.client.email: 'No Found' }</td>
-              <td>{e.createDate.slice(0, 10)}  -  {e.createDate.slice(11,16)} hrs</td>
-              <td> $ {e.ammount}</td>
-           
-              <td>
-                <select
-                  className="form-select"
-                  onChange={(value) => handlerStatus(value, e.id) }name= 'seleccionar' >
-                          <option disabled="disabled" selected="true" > {e.status}</option>
+              <th>Detalles:</th>
+            </tr>
+          </thead>
 
-                  <option value="creada"> creada</option>
-                  <option value="procesando"> procesando</option>
-                  <option value="cancelada"> cancelada</option>
-                  <option value="completa"> completa</option>
-                </select>
-              </td>
+          {orders ? (
+            orders.map((e) => (
+              <tbody>
+                <td>{e.id}</td>
+                <td>{e.client ? e.client.name : 'User'} {'  '}{e.client ? e.client.lastname : 'No Found '} </td>
+                <td>{e.client ? e.client.email : 'No Found'}</td>
+                <td>{e.createDate.slice(0, 10)}  -  {e.createDate.slice(11, 16)} hrs</td>
+                <td> $ {e.ammount}</td>
 
-              <td>
-                <Button color="primary" > Ver Mas </Button>
-              </td>
-            </tbody>
-          ))
-        ) : (
-          <div>Cargando </div>
-        )}
-      </table>
+                <td>
+                  <select
+                    className="form-select"
+                    onChange={(value) => handlerStatus(value, e.id)} name='seleccionar' >
+                    <option disabled="disabled" selected="true" > {e.status}</option>
+
+                    <option value="creada"> creada</option>
+                    <option value="procesando"> procesando</option>
+                    <option value="cancelada"> cancelada</option>
+                    <option value="completa"> completa</option>
+                  </select>
+                </td>
+
+                <td>
+                  <Button color="primary" > Ver Mas </Button>
+                </td>
+              </tbody>
+            ))
+          ) : (
+            <div>Cargando </div>
+          )}
+        </table>
+      </div>
     </div>
   );
 }
