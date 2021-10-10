9658c99cc90211b06c1bdf4250a3dd3ca6231ce3
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2021-10-10 00:12:05 -0500

    Tables de categoria y dieta en/admin/filters

--- front/src/App.js
+++ front/src/App.js
@@ -15,6 +15,7 @@ import Login from './Components/Login';
 import Sidebar from './Components/AdminSideBar';
 import UsersAdminDetail from './Components/UsersAdminDetail'
 import OrderAdminDetail from './Components/OrdersAdminDetail';
+import AdminDietAndCategory from './Components/TableDietAndCategory';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
     <Route exact path='/Admin' component={Creator}/>
     <Route exact path='/Admin/user' component={UsersAdminDetail}/>
     <Route exact path='/Admin/orders' component={OrderAdminDetail}/>
+    <Route exact path='/Admin/filters' component={AdminDietAndCategory}/>
 
 
     </Switch>
