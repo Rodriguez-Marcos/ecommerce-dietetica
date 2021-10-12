b3c52266bfca33e4a7ca3e62880117cf0b83ca57
rociojalil <rojalilparedes@gmail.com>
2021-10-12 15:22:25 -0300

    deployyyyy

--- back/src/index.js
+++ back/src/index.js
@@ -2,13 +2,15 @@ import app from './app.js';
 import { sequelize } from './database/db.js'
 import '@babel/polyfill'
 // const { DB_PORT } = process.env;
+require ('dotenv').config();
+const { PORT } = process.env;
+
 
   
-
 function main() {
     sequelize.sync({ force: false }).then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('listening on port 3001'); // eslint-disable-line no-console
+        app.listen(PORT , () => {
+            console.log( PORT); // eslint-disable-line no-console
 
         });
     })
