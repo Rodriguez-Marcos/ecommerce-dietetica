54a089ca365959a508afd4457d34d42b66ef9b54
JuanLertora <juan.lertora@hotmail.com>
2021-10-12 16:41:06 -0300

    act

--- front/src/App.js
+++ front/src/App.js
@@ -43,11 +43,6 @@ function App() {
     <Route exact path='/Login' component={Login}/>
     <Route exact path='/Admin' component={Creator}/>
 
-    <Route exact path='/Admin/user' component={UsersAdminDetail}/>
-    <Route exact path='/Admin/orders' component={OrderAdminDetail}/>
-    <Route exact path='/Admin/orders' component={TableOrders}/>
-    <Route exact path='/Admin/filters' component={AdminDietAndCategory}/>
-
     <Route exact path='/newadress' component={CreateAdress}/>
     <Route exact path='/Admin' render={() => isAdmin ? <Creator></Creator>: <Redirect to='/home'/>}/>
     <Route exact path='/Admin/user'  render={() => isAdmin ? <UsersAdminDetail/>: <Redirect to='/home'/>}/>
