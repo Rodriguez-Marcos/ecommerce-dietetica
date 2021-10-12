2c3d06e12ded935a42abaadf3b5fae9ec6f92d8d
rociojalil <rojalilparedes@gmail.com>
2021-10-12 20:02:51 -0300

    asjdsajkd

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 import { sequelize } from './database/db.js'
-import '@babel/polyfill'
+// import '@babel/polyfill'
 // require("babel/polyfill");
 import "core-js/stable";
 
