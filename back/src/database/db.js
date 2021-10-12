06be9fd51f64fdf202c008d8c55a0bd98a7b3f75
rociojalil <rojalilparedes@gmail.com>
2021-10-12 02:31:04 -0300

    env2

--- back/src/database/db.js
+++ back/src/database/db.js
@@ -9,7 +9,7 @@ export const sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 5432,
+        port: 6013,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
