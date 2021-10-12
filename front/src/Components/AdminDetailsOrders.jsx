fdd610e5b4fae9468610f3a52fdfb877af3ea245
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2021-10-11 21:01:52 -0500

    Ruta /admin/orders para mirar las ordenes

---
+++ front/src/Components/AdminDetailsOrders.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalGrup,
+} from "reactstrap";
+
+export default function AdminDetailsOrders({ input , closeModal}) {
+  console.log("input", input);
+
+  return (
+    <div>
+      <Modal isOpen={input.modal}>
+        <ModalHeader>
+          DETALLES DEL PEDIDO NRO: {input.id}{" "}
+          <b class="text-success"> {input.status}</b>
+        </ModalHeader>
+        <ModalBody>
+          <div>
+            <h6>
+              <b>Nombres del Cliente:</b> {input.clientName}
+              {input.clientLastname}
+            </h6>
+          </div>
+          <div>
+            <h6>
+              <b>Correo Electronico:</b> {input.clientMail}
+            </h6>
+          </div>
+          <div>
+            <h6>
+              <b>Nro de Teléfono:</b> {input.clientPhone}
+            </h6>
+          </div>
+          <div>
+            <h6>
+              <b>Dirección de envio: </b> {input.direccion}
+            </h6>
+          </div>
+
+          <table className="table">
+            <thead>
+              <th>Nombre del Producto</th>
+              <th> Cant.</th>
+              <th> V.Unit.</th>
+              <th> V.Total. </th>
+            </thead>
+
+            {input.productName.map((e) => (
+              <tbody>
+                <td> {e.name}</td>
+                <td> {e.products_order.quantity}</td>
+                <td class="text-left"> $ {e.price}</td>
+                <td class="text-left"> $ {e.products_order.total}</td>
+              </tbody>
+            ))}
+            <tfoot>
+              <tr>
+                <td>Total: </td>
+                <td />
+                <td />
+                <td> $ {input.total}</td>
+              </tr>
+            </tfoot>
+          </table>
+        </ModalBody>
+        <ModalFooter>
+      
+          <Button color="danger" onClick ={(e)=>closeModal(e)}>Salir</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
--- front/src/Components/TableOrders.jsx
+++ front/src/Components/TableOrders.jsx
@@ -1,21 +1,72 @@
-import { React } from "react"; import "bootstrap";
-import {
-  Button,
-} from "reactstrap";
+import { React } from "react";
+import "bootstrap";
+import { Button } from "reactstrap";
 import Topbar from "./AdminTopBar";
 import Sidebar from "./AdminSideBar";
-import './TableOrders.css'
+import AdminDetailsOrders from "./AdminDetailsOrders";
+import "./TableOrders.css";
+import { useState } from "react";
 
 export default function TableOrders({ orders, handlerStatus }) {
+  const [state, setstate] = useState({
+
+    clientName: "",
+    clientLastname: "",
+    clientPhone: "",
+    clientMail: "",
+    productName: [], 
+    direccion: "",
+    id:'', 
+    date: "",
+    total:'',
+    status: '', 
+    modal: false,
+  });
+
+  function handlerDetails(e) {
+    console.log('esto es e',e)
+   
+
+        setstate({
+          ...state,
+         modal:true, 
+         id: e.id,
+         direccion: e.shippingAddress,
+         date: e.createDate,
+         total: e.ammount, 
+         status: e.status,
+         clientName: e.client? e.client.name : 'No Found',
+         clientLastname: e.client? e.client.lastname : 'No Found',
+         clientPhone: e.client? e.client.phone : 'No Found',
+         clientMail: e.client? e.client.email : 'No Found',
+         productName: e.products ? e.products.filter(e=> e.name) : 'No Found'
+
+        })
+
+       
+  }
+  function closeModal(e){
+
+    setstate({
+      ...state,
+     modal:false, 
+    
+
+    })
+
+  }
+
+  
+
   return (
     <div className="ordersContenedor">
       <Topbar />
       <div className="ordersTable-Sidebar">
         <Sidebar />
-        <table className="table1">
+        <table className={` ${"table"} `}>
           <thead>
             <tr>
-              <th>Nro: </th>
+              <th>Numero: </th>
               <th>Datos del cliente:</th>
               <th>Email</th>
               <th>Fecha de pedido:</th>
@@ -30,16 +81,26 @@ export default function TableOrders({ orders, handlerStatus }) {
             orders.map((e) => (
               <tbody>
                 <td>{e.id}</td>
-                <td>{e.client ? e.client.name : 'User'} {'  '}{e.client ? e.client.lastname : 'No Found '} </td>
-                <td>{e.client ? e.client.email : 'No Found'}</td>
-                <td>{e.createDate.slice(0, 10)}  -  {e.createDate.slice(11, 16)} hrs</td>
+                <td>
+                  {e.client ? e.client.name : "User"} {"  "}
+                  {e.client ? e.client.lastname : "No Found "}{" "}
+                </td>
+                <td>{e.client ? e.client.email : "No Found"}</td>
+                <td>
+                  {e.createDate.slice(0, 10)} - {e.createDate.slice(11, 16)} hrs
+                </td>
                 <td> $ {e.ammount}</td>
 
                 <td>
                   <select
                     className="form-select"
-                    onChange={(value) => handlerStatus(value, e.id)} name='seleccionar' >
-                    <option disabled="disabled" selected="true" > {e.status}</option>
+                    onChange={(value) => handlerStatus(value, e.id)}
+                    name="seleccionar"
+                  >
+                    <option disabled="disabled" selected="true">
+                      {" "}
+                      {e.status}
+                    </option>
 
                     <option value="creada"> creada</option>
                     <option value="procesando"> procesando</option>
@@ -49,7 +110,7 @@ export default function TableOrders({ orders, handlerStatus }) {
                 </td>
 
                 <td>
-                  <Button color="primary" > Ver Mas </Button>
+                  <Button color="primary" onClick={()=>handlerDetails(e)}> Ver Mas </Button>
                 </td>
               </tbody>
             ))
@@ -58,6 +119,7 @@ export default function TableOrders({ orders, handlerStatus }) {
           )}
         </table>
       </div>
+      <AdminDetailsOrders  input= {state} closeModal ={closeModal}  />
     </div>
   );
 }
