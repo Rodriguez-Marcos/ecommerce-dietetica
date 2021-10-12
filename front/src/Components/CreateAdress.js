f06b504007bb1fe610b6c7903e46dc5c6304064a
pablo agustin <39503400+JDFgamer@users.noreply.github.com>
2021-10-12 15:59:33 -0300

    se agrego inputs a payment

--- front/src/Components/CreateAdress.js
+++ front/src/Components/CreateAdress.js
@@ -27,7 +27,7 @@ export default function Adress() {
         })
     }
 
-    useEffect(() => { }, [payment])
+    /* useEffect(() => { }, [payment]) */
     async function handleSubmit(event) {
         event.preventDefault()
         if (!input.direccion || !input.altura || !input.ciudad || !input.provincia || !input.numero) { alert('Debes llenar todos los campos') }
