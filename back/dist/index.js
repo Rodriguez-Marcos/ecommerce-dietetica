469f2749bab36a33d9304850141812798dc9e84c
rociojalil <rojalilparedes@gmail.com>
2021-10-12 20:04:52 -0300

    dkfjdskf

--- back/dist/index.js
+++ back/dist/index.js
@@ -4,12 +4,11 @@ var _app = _interopRequireDefault(require("./app.js"));
 
 var _db = require("./database/db.js");
 
-require("@babel/polyfill");
-
 require("core-js/stable");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+// import '@babel/polyfill'
 // require("babel/polyfill");
 // const { DB_PORT } = process.env;
 require('dotenv').config();
