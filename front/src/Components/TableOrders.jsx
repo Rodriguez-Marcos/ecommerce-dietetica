dfbde0884407873bb0fb79d0ea7f6870690931b5
JuanLertora <juan.lertora@hotmail.com>
2021-10-15 01:23:48 -0300

    act

--- front/src/Components/TableOrders.jsx
+++ front/src/Components/TableOrders.jsx
@@ -43,7 +43,7 @@ export default function TableOrders() {
     e.preventDefault();
     
       if (e.target.value === "todos") {
-        dispatch(getOrders(window.localStorage.jwt));
+        dispatch(getOrders(window.localStorage.jwt,order));
       } else {
       
         dispatch(getFilterStatus(e.target.value, token));
