31fc90d5e12d0216f613e7261b16155f950e01f1
rociojalil <rojalilparedes@gmail.com>
2021-10-17 02:20:26 -0300

    agregado control de errores form pero css se baja

--- front/src/Components/formbefore.js
+++ front/src/Components/formbefore.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 // import Styles from './payment.module.css'
 import styles from './formbefore.module.css'
 import NavBar from './NavBar';
@@ -8,14 +8,87 @@ import GoogleMaps from "simple-react-google-maps"
 import { StyleSharp } from '@material-ui/icons';
 import Cookies from "universal-cookie";
 import { useDispatch, useSelector } from "react-redux";
+import { DataContext } from "../Contexts/DataProvider"
 import getCart from "../Utils/getCart";
 import axios from 'axios'
 import { useHistory } from "react-router";
 import swal from "sweetalert";
+import { Link } from 'react-router-dom';
 
 export default function Pending(){
     const myStorage = window.localStorage;
+    const value = useContext(DataContext)
+  const [carrito, setCarrito] = value.carrito;
+  const [total, setTotal] = value.total;
+  const [menu, setMenu] = value.menu;
+  const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
 
+  function validateForm(input) {
+    let errors = {};
+    if (!input.calle) {
+        errors.calle = "Escribe una calle";
+    } else {
+        errors.calle = "";
+    }
+    if (!input.altura) {
+        errors.altura = "Escribe una altura";
+    } else if (!/\d{1,2} - \d{1,2}/g.test(input.altura)) {
+        errors.altura =
+            "La altura deben ser un máximo de 6 dígitos";
+    } else {
+        errors.altura = "";
+    }
+    if (!input.barrio) {
+        errors.barrio = "Escribe un barrio";
+    } else {
+        errors.barrio = "";
+    }
+    if (!input.otros) {
+        errors.otros = "Escribe datos adicionales de dirección";
+    } else {
+        errors.otros = "";
+    }
+    if (!input.codigo) {
+        errors.codigo = "Escribe un código postal";
+    } else if (!/\d{1,2} - \d{1,2}/g.test(input.codigo)) {
+        errors.codigo =
+            "El código postal no debe ser mayor a 10 dígitos";
+    } else {
+        errors.codigo = "";
+    }
+    
+    if (!input.numero) {
+        errors.numero = "Escribe un número de teléfono";
+    } else if (!/\d{1,2}-\d{1,2}/g.test(input.numero)) {
+        errors.numero=
+            "Escribe un teléfono con prefijo válido";
+    } else {
+        errors.numero = "";
+    }
+   
+    return errors;
+};
+function handleInput(e) {
+  setInput({
+      ...input,
+      [e.target.name]: e.target.value
+  })
+  setErrors(
+      validateForm({
+          ...input,
+          [e.target.name]: e.target.value,
+      })
+      );
+    }
+
+    function onFocus(ev) {
+      setTouched({
+          ...touched,
+          [ev.target.name]: true,
+      });
+  }
+     
 
     let [input, setInput]=useState({
         calle: ' ',
@@ -26,12 +99,12 @@ export default function Pending(){
         numero: ' ',
     })
 
-    function handleAdress(event) {
-        setInput({
-            ...input,
-            [event.target.name]: event.target.value
-        })
-    }
+    // function handleAdress(event) {
+    //     setInput({
+    //         ...input,
+    //         [event.target.name]: event.target.value
+    //     })
+    // }
     // useEffect(() => {
     //     emptycart(myStorage.getItem('jwt'))
     // }, [])
@@ -126,46 +199,124 @@ let { isLogin, token, comodin } = useSelector(state => state.reducerPablo)
             <p className={styles.ultimo2}>Por favor seleccione: </p>
             <div className={styles.form}>
             <div class="form-check form-check-inline">
+              
       <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"></input>
       <label class="form-check-label" for="inlineCheckbox1">Envío a domicilio:</label>
       </div>
             <form>
   <div class="form-row">
     <div class="form-group col-md-4">
-      <label for="inputEmail4">Calle:</label>
-      <input type="text" class="form-control" name='calle' value={input.calle} onChange={handleAdress} ></input>
+      <label for="inputCity">Calle:</label>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="calle"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.calle}
+                    ></input>
+                    {errors.calle && touched.calle && (
+                    <p className={styles.errorMsg}>{errors.calle}</p>
+                    )}
+
+      {/* <input type="text" class="form-control" name='calle' value={input.calle} onChange={handleAdress} ></input> */}
     </div>
   </div>
   <div class="form-group col-md-4">
       <label for="inputCity">Altura:</label>
-      <input type="text" class="form-control" name='altura' value={input.altura} onChange={handleAdress} ></input>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="altura"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.altura}
+                    ></input>
+                    {errors.altura && touched.altura && (
+                    <p className={styles.errorMsg}>{errors.altura}</p>
+                    )}
+
+{/* 
+      <input type="text" class="form-control" name='altura' value={input.altura} onChange={handleAdress} ></input> */}
     </div>
   <div class="form-group col-md-4">
       <label for="inputCity">Barrio:</label>
-      <input type="text" class="form-control" name='barrio' value={input.barrio} onChange={handleAdress}  ></input>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="barrio"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.barrio}
+                    ></input>
+                    {errors.barrio && touched.barrio && (
+                    <p className={styles.errorMsg}>{errors.barrio}</p>
+                    )}
+      {/* <input type="text" class="form-control" name='barrio' value={input.barrio} onChange={handleAdress}  ></input> */}
     </div>
     <div class="form-group col-md-4">
       <label for="inputZip">Otros:</label>
-      <input type="text" class="form-control" name='otros' value={input.otros} onChange={handleAdress} ></input>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="otros"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.otros}
+                    ></input>
+                    {errors.otros && touched.otros && (
+                    <p className={styles.errorMsg}>{errors.otros}</p>
+                    )}
+      {/* <input type="text" class="form-control" name='otros' value={input.otros} onChange={handleAdress} ></input> */}
     </div>
     <div class="form-group col-md-4">
       <label for="inputZip">Código postal:</label>
-      <input type="text" class="form-control" name='codigo' value={input.codigo} onChange={handleAdress}  ></input>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="codigo"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.codigo}
+                    ></input>
+                    {errors.codigo && touched.codigo && (
+                    <p className={styles.errorMsg}>{errors.codigo}</p>
+                    )}
+      {/* <input type="text" class="form-control" name='codigo' value={input.codigo} onChange={handleAdress}  ></input> */}
     </div>
   <div class="form-row">
     <div class="form-group col-md-4">
       <label for="inputCity">Número de contacto:</label>
-      <input type="text" class="form-control" name='numero' value={input.numero} onChange={handleAdress} ></input>
+      <input
+                    type="text"
+                    class="form-control"
+                    name="numero"
+                    // a todos los input le pongo handleInput para que vaya al estado
+                    onChange={handleInput}
+                    required='required'
+                    onFocus={onFocus}
+                    value={input.numero}
+                    ></input>
+                    {errors.numero && touched.numero && (
+                    <p className={styles.errorMsg}>{errors.numero}</p>
+                    )}
+      {/* <input type="text" class="form-control" name='numero' value={input.numero} onChange={handleAdress} ></input> */}
     </div>
   </div>
   <br/>
   <button type="submit" class="btn btn-primary" onClick={handleSubmit}>Cargar</button>
 </form>
 </div>
-
-
-
-
 
 
                 <div className={styles.google}>
@@ -199,7 +350,8 @@ let { isLogin, token, comodin } = useSelector(state => state.reducerPablo)
                 </div>
                 <button type="button" class="btn btn-success" onClick={handleCompra}>Comprar</button>
                 {/* <button class="btn btn-primary" color="green" type="button">Comprar</button> */}
-                <button type="button" class="btn btn-dark">Volver al carrito</button>
+          
+                <button type="button" class="btn btn-dark" onClick={()=>{setMenu(true)}} >Volver al carrito</button>
                
                 </div>
 
