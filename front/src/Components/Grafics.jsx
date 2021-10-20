e2f0b0b36bea7193ce1c6847a115fc19e8180454
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2021-10-20 13:06:48 -0500

    Funcion generadora de colorea aleatorios

--- front/src/Components/Grafics.jsx
+++ front/src/Components/Grafics.jsx
@@ -12,53 +12,82 @@ export default function Grafics() {
   // estados
   const [order, setOrder] = useState("");
 
-
   useEffect(() => {
     dispatch(getOrders(token, order));
   }, []);
-    const [state, setState] = useState({
+  const [state, setState] = useState({
+    order_name: [], 
     order_ids: [],
     order_ammounts: [],
+    colors: [],
+
   });
 
-
-    function Dates (){
- setState({
-        ...state, 
-        order_ids: orders.map((e) => e.id).reverse() ,
-        order_ammounts: orders.map((e) => e.ammount).reverse(),
-
-    })
-
-    }
- 
- const data = {
-
-    labels: state.order_ids,    
+  const data = {
+    labels: state.order_ids,
     datasets: [
       {
-        label: "Dinero", 
-        backgroundColor: "rgb(196, 188, 38)",
+        label: "Dinero $",
+        backgroundColor: ["rgb(155, 105, 0)", 'blue', 'black', 'green', 'red', 'yellow'],
         borderColor: "black",
         borderWidth: 2,
-        hoverBackgroundColor: "rgba(0, 255,45,24,0.2)",
-        hoverBorderColor: "#fff000",
+        hoverBackgroundColor: "rgb(131, 97, 24 )",
+        hoverBorderColor: "black",
         data: state.order_ammounts,
       },
     ],
   };
- 
+
   const opciones = {
     maintainAspectRatio: false,
     responsive: true,
   };
+  function generateCharacter() {
+    let characters = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ];
+    let number = (Math.random() * 15).toFixed(0);
+    return characters[number];
+  }
 
+  function GenerateColors() {
+    let colors = [];
+    for (let i = 0; i > orders.id; i++) {
+      colors.push(generateCharacter());
+    }
+    return colors;
+  }
+
+  function Dates() {
+    setState({
+      ...state,
+      order_ids: orders.map((e) => e.id).reverse(),
+      order_ammounts: orders.map((e) => e.ammount).reverse(),
+      colors: GenerateColors(),
+    });
+  }
+  console.log(orders);
   return (
     <div style={{ width: "100%", height: "500px" }}>
       <h2>Graficos de compras por cliente.</h2>
 
       <Bar data={data} options={opciones} />
-      <button onClick ={()=>Dates()}> Ver</button>
+      <button onClick={() => Dates()}> Ver</button>
     </div>
   );
 }
