1be3e74e1f7e80efe4fdc758503195e7faf36341
fsesin <faridsesin@gmail.com>
2021-10-22 12:25:32 -0500

    no mas

--- back/src/routes/payment.js
+++ back/src/routes/payment.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 import {createOrder} from '../controllers/order'
+import {emptyCart} from '../controllers/cart'
 import payment from '../controllers/payment'
 
 const router = Router()
@@ -7,5 +8,6 @@ const router = Router()
 
 router.post('/', payment)
 router.get('/success', createOrder)
+router.get('/pending', emptyCart)
 
 export default router;
